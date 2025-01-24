a37ef70a6e049b95517959ceed83843aca8744e9
Abhinish Kumar <141950751+Abhinish-Kumar@users.noreply.github.com>
2025-01-24 12:59:56 +0530

    Update About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,38 @@
-
+import "./About.css"
 
 function About() {
-            return <h2>About page</h2>;
+            return <>
+            <div id="imageContainerAbout">
+            <img src="https://wallpapercave.com/wp/wp7488433.jpg" alt="event image" />
+            <h2 className="slim-text ">ABOUT US </h2>
+            </div>
+            <div id="contactAbout">
+<p >SPEAK TO US TODAY</p>
+<p>TE: +44 (0) 208 606 9636</p>
+            </div>
+            <div id="welcomeAbout">
+    <p id="welcomeTitle" className="slim-text">WELCOME TO</p>
+    <p id="mainTitle" className="slim-text">MAGNA PRODUCTION</p>
+    <p id="tagline">Tried. Trusted. Inspired</p>
+    
+    <div className="blog-post">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore dicta eius necessitatibus ratione odit dolorem! Quis deserunt sit labore rem, cupiditate praesentium id repellendus .</p>
+    </div>
+    
+    <div className="blog-post ">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo unde ipsa vitae praesentium dolorem aperiam hic nisi amet neque necessitatibus! Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore dicta eius necessitatibus ratione odit dolorem! Quis deserunt sit labore rem, cupiditate praesentium id repellendus ex.</p>
+    </div>
+    
+    <div className="blog-post">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore dicta eius necessitatibus ratione odit dolorem! Quis deserunt sit labore rem, cupiditate praesentium id repellendus.</p>
+    </div>
+    
+    <div className="blog-post">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore dicta eius necessitatibus ratione odit dolorem! Quis deserunt sit labore rem, cupiditate praesentium id repellendus.</p>
+    </div>
+</div>
+
+            </>;
           }
           
           export default About;
